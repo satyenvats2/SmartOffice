94a53e86465436f5e3f6b995c81102211715123a
pranavmahajan21 <pranav@motifworks.com>
2014-04-26 13:22:11 +0530

    function for round images

--- src/com/mw/smartoff/services/GlobalVariable.java
+++ src/com/mw/smartoff/services/GlobalVariable.java
@@ -3,6 +3,10 @@ package com.mw.smartoff.services;
 import java.util.List;
 
 import android.app.Application;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Path;
+import android.graphics.Rect;
 
 import com.mw.smartoff.model.Email;
 import com.mw.smartoff.model.Meeting;
@@ -80,5 +84,26 @@ public class GlobalVariable extends Application {
 				meetingPO.getString("subject"),
 				meetingPO.getString("description"),
 				meetingPO.getString("location"), meetingPO.getDate("startTime"));
+	}
+	
+	public static Bitmap getRoundedShape(Bitmap scaleBitmapImage) {
+		int targetWidth = 150;
+		int targetHeight = 150;
+		Bitmap targetBitmap = Bitmap.createBitmap(targetWidth, targetHeight,
+				Bitmap.Config.ARGB_8888);
+
+		Canvas canvas = new Canvas(targetBitmap);
+		Path path = new Path();
+		path.addCircle(((float) targetWidth - 1) / 2,
+				((float) targetHeight - 1) / 2,
+				(Math.min(((float) targetWidth), ((float) targetHeight)) / 2),
+				Path.Direction.CCW);
+
+		canvas.clipPath(path);
+		Bitmap sourceBitmap = scaleBitmapImage;
+		canvas.drawBitmap(sourceBitmap, new Rect(0, 0, sourceBitmap.getWidth(),
+				sourceBitmap.getHeight()), new Rect(0, 0, targetWidth,
+				targetHeight), null);
+		return targetBitmap;
 	}
 }
