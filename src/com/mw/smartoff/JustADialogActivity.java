5a11c4d3dff3cde79c8491ba56232e9704d8d5e2
pranavmahajan21 <pranav@motifworks.com>
2014-04-30 16:33:20 +0530

    intent issue resolved

--- src/com/mw/smartoff/JustADialogActivity.java
+++ src/com/mw/smartoff/JustADialogActivity.java
@@ -13,52 +13,53 @@ public class JustADialogActivity extends Activity {
 	CreateDialog createDialog;
 	AlertDialog.Builder alertDialogBuilder;
 	AlertDialog alertDialog;
+	Intent nextIntent;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 
-        setContentView(R.layout.broadcast_alert);
-        Intent intent = getIntent();
-        Bundle extras = intent.getExtras();
-        int notificationType = -1;
-        if (extras != null) {
-            notificationType = extras.getInt("type");
-            System.out.println(">>>>>>>" + notificationType);
-        }
+		setContentView(R.layout.broadcast_alert);
+		Intent previousIntent = getIntent();
+		Bundle extras = previousIntent.getExtras();
+		int notificationType = -1;
+		if (extras != null) {
+			notificationType = extras.getInt("type");
+			System.out.println(">>>>>>>" + notificationType);
+		}
 
-        final Intent nextIntent = null;
-        String dialogNoteTitle = null;
-        String dialogNoteMessage = null;
-        switch (notificationType){
-            case 0:
-                nextIntent = new Intent(this, DisplayEmailActivity.class);
-                dialogNoteTitle = "New Email";
-                dialogNoteMessage = "You have a new Email";
-                break;
-            case 1:
-                nextIntent = new Intent(this, DisplayMeetingActivity.class);
-                dialogNoteTitle = "New Meeting Invite";
-                dialogNoteMessage = "You have a new Meeting Invite";
-                break;
-            case 2:
-                nextIntent = new Intent(this, DisplayEmailActivity.class);
-                dialogNoteTitle = "New Message";
-                dialogNoteMessage = "You have a new Message";
-                break;
-            default:
-                break;
-        }
+		String dialogNoteTitle = null;
+		String dialogNoteMessage = null;
 
-	    createDialog = new CreateDialog(this);
+		switch (notificationType) {
+		case 0:
+			nextIntent = new Intent(this, DisplayEmailActivity.class);
+			dialogNoteTitle = "New Email";
+			dialogNoteMessage = "You have a new Email";
+			break;
+		case 1:
+			nextIntent = new Intent(this, DisplayMeetingActivity.class);
+			dialogNoteTitle = "New Meeting Invite";
+			dialogNoteMessage = "You have a new Meeting Invite";
+			break;
+		case 2:
+			nextIntent = new Intent(this, DisplayEmailActivity.class);
+			dialogNoteTitle = "New Message";
+			dialogNoteMessage = "You have a new Message";
+			break;
+		default:
+			break;
+		}
 
-		alertDialogBuilder = createDialog.createAlertDialog(dialogNoteTitle, dialogNoteMessage,
-				false);
+		createDialog = new CreateDialog(this);
+
+		alertDialogBuilder = createDialog.createAlertDialog(dialogNoteTitle,
+				dialogNoteMessage, false);
 		alertDialogBuilder.setPositiveButton("Read now",
 				new DialogInterface.OnClickListener() {
 					public void onClick(DialogInterface dialog, int id) {
 						dialog.dismiss();
-			            startActivity(nextIntent);
+						startActivity(nextIntent);
 					}
 				});
 		alertDialogBuilder.setNegativeButton("Read it later",
@@ -67,7 +68,7 @@ public class JustADialogActivity extends Activity {
 						dialog.dismiss();
 					}
 				});
-		
+
 		alertDialog = alertDialogBuilder.create();
 		alertDialog.show();
 
