99cd54f92a08ecbb07f360ac98411b33bd773251
pranavmahajan21 <pranav@motifworks.com>
2014-05-17 19:10:20 +0530

    non important changes

--- src/com/mw/smartoff/DisplayAttendanceActivity.java
+++ src/com/mw/smartoff/DisplayAttendanceActivity.java
@@ -1,16 +1,67 @@
 package com.mw.smartoff;
 
-import com.mw.smartoffice.R;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 
+import com.mw.smartoff.DAO.AttendanceDAO;
+import com.mw.smartoff.services.AttendanceService;
+import com.mw.smartoffice.R;
+
 public class DisplayAttendanceActivity extends Activity {
+
+	Date selectedDate;
+	AttendanceDAO dao;
+	Intent serviceIntent;
+
+	private void findThings() {
+	}
+
+	private void initThings() {
+		dao = new AttendanceDAO(this);
+		selectedDate = new Date();
+		serviceIntent = new Intent(this, AttendanceService.class);
+	}
+
+	private void roundOffDate() {
+	}
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-setContentView(R.layout.attendance_fragment);
+		setContentView(R.layout.display_attendance);
+		findThings();
+		initThings();
+
+		startService(serviceIntent);
 	}
-	
+
+	@SuppressLint("SimpleDateFormat")
+	private Date formatDate(Date date) {
+
+		SimpleDateFormat formatter = new SimpleDateFormat("dd MMM yyyy zzzz");
+
+		String dateStr = formatter.format(date);
+
+		Date date2 = null;
+		try {
+			date2 = formatter.parse(dateStr);
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+		return date2;
+	}
+
+	Date getDate(Date date, int days) {
+		Calendar cal = Calendar.getInstance();
+		cal.setTime(date);
+		cal.add(Calendar.DAY_OF_MONTH, days);
+		return cal.getTime();
+	}
 }
---
+++ src/com/mw/smartoff/services/AttendanceService.java
@@ -0,0 +1,54 @@
+package com.mw.smartoff.services;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.mw.smartoff.DAO.AttendanceDAO;
+import com.mw.smartoff.DAO.EmailDAO;
+import com.mw.smartoff.extras.GlobalVariable;
+import com.mw.smartoff.model.Email;
+import com.parse.ParseObject;
+import com.parse.ParseUser;
+
+import android.app.IntentService;
+import android.content.Intent;
+import android.support.v4.content.LocalBroadcastManager;
+
+public class AttendanceService extends IntentService {
+
+	AttendanceDAO dao;
+	GlobalVariable globalVariable;
+
+	public AttendanceService() {
+		super("AttendanceService");
+	}
+
+	@Override
+	public void onCreate() {
+		super.onCreate();
+		dao = new AttendanceDAO(this);
+		globalVariable = (GlobalVariable) this.getApplicationContext();
+	}
+
+	@Override
+	protected void onHandleIntent(Intent arg0) {
+		List<Email> emailList = new ArrayList<Email>();
+		List<ParseObject> emailPOList = dao.getEmailsForUser(ParseUser
+				.getCurrentUser().getEmail());
+		if (emailPOList != null) {
+			for (int i = 0; i < emailPOList.size(); i++) {
+				ParseObject tempEmailPO = emailPOList.get(i);
+				Email tempEmail = globalVariable.convertPOtoEmail(tempEmailPO);
+				emailList.add(tempEmail);
+			}
+			globalVariable.setEmailList(emailList);
+		}
+	}
+
+	@Override
+	public void onDestroy() {
+		super.onDestroy();
+		Intent nextIntent = new Intent("new_email");
+		LocalBroadcastManager.getInstance(this).sendBroadcast(nextIntent);
+	}
+}
